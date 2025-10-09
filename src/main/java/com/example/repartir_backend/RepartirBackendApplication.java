d558a6fd2d5a7f989d70b39c86f3267bd770062b
jimlecracken14 <jimlecracken13@gmail.com>
2025-10-09 15:27:50 +0000

    initial commit

---
+++ src/main/java/com/example/repartir_backend/RepartirBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.repartir_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepartirBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepartirBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repartir_backend/RepartirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repartir_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepartirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
