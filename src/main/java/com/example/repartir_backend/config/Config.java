3e1db58d6feb4d6a0fc1d9cb27438f55ea868788
jimlecracken14 <jimlecracken13@gmail.com>
2025-10-13 10:21:09 +0000

    bugs mineurs regler

--- src/main/java/com/example/repartir_backend/config/Config.java
+++ src/main/java/com/example/repartir_backend/config/Config.java
@@ -8,15 +8,4 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * Classe de configuration pour les beans de l'application.
  */
-@Configuration
-public class Config {
-    /**
-     * Crée un bean pour l'encodeur de mots de passe.
-     * Utilise BCrypt pour un hachage sécurisé des mots de passe.
-     * @return Une instance de PasswordEncoder.
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-}
+
